0c2f9289b6ab1312d7f325137401193a21282fb8
jg715 <jgajowiecki714@gmail.com>
2025-10-02 11:00:22 +0200

    02.10.2025

--- Ore dash test1/Assets/scripts/PlayerMovement.cs
+++ Ore dash test1/Assets/scripts/PlayerMovement.cs
@@ -1,51 +1,112 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
+
 {
-    [SerializeField] private float speed = 3f;
 
-    private Rigidbody2D body;
-    private Vector2 axisMovement;
+    [Header("Movement Settings")]
 
-    // Start is called before the first frame update
+    public float moveSpeed = 5f;
+
+    public float jumpForce = 10f;
+
+    [Header("Jump Cooldown")]
+
+    public float jumpCooldown = 3f;
+
+    private float lastJumpTime = -Mathf.Infinity;
+
+    [Header("Dash Settings")]
+
+    public float dashSpeed = 15f;
+
+    public float dashDuration = 0.2f;
+
+    private bool isDashing = false;
+
+    private bool canDash = true;
+
+    private float horizontalInput;
+
+    private Rigidbody2D rb;
+
     void Start()
+
     {
-        body = GetComponent<Rigidbody2D>();
+
+        rb = GetComponent<Rigidbody2D>();
+
     }
 
-    // Update is called once per frame
     void Update()
-    {
-        axisMovement.x = Input.GetAxisRaw("Horizontal");
-        axisMovement.y = Input.GetAxisRaw("Vertical");
-    }
 
-    private void FixedUpdate()
     {
-        Move();
-    }
 
-    private void Move()
-    {
-        body.linearVelocity = axisMovement.normalized * speed;
-        CheckForFlipping();
-    }
+        if (isDashing) return;
 
-    private void CheckForFlipping()
-    {
-        bool movingLeft = axisMovement.x < 0;
-        bool movingRight = axisMovement.x > 0;
+        horizontalInput = Input.GetAxisRaw("Horizontal");
 
-        if (movingLeft)
+        // Skok z cooldownem
+
+        if (Input.GetButtonDown("Jump") && Time.time >= lastJumpTime + jumpCooldown)
+
         {
-            transform.localScale = new Vector3(-1f, transform.localScale.y);
+
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+
+            lastJumpTime = Time.time;
+
+            Debug.Log("Skok! (Cooldown)");
+
         }
 
-        if (movingRight)
+        // Dash
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) && canDash)
+
         {
-            transform.localScale = new Vector3(1f, transform.localScale.y);
-        }        
+
+            StartCoroutine(Dash());
+
+        }
+
     }
+
+    void FixedUpdate()
+
+    {
+
+        if (isDashing) return;
+
+        rb.linearVelocity = new Vector2(horizontalInput * moveSpeed, rb.linearVelocity.y);
+
+    }
+
+    System.Collections.IEnumerator Dash()
+
+    {
+
+        isDashing = true;
+
+        canDash = false;
+
+        float originalGravity = rb.gravityScale;
+
+        rb.gravityScale = 0f;
+
+        rb.linearVelocity = new Vector2(transform.localScale.x * dashSpeed, 0f);
+
+        yield return new WaitForSeconds(dashDuration);
+
+        rb.gravityScale = originalGravity;
+
+        isDashing = false;
+
+        yield return new WaitForSeconds(0.5f);
+
+        canDash = true;
+
+    }
+
 }
+
