c94e118bb5d75d32a98c7f4675a9eaeb67ad319f
jg715 <jgajowiecki714@gmail.com>
2025-10-16 11:04:20 +0200

    16.10.2025

---
+++ Ore dash test1/Assets/scripts/npc/NPCDialogue.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+using TMPro;  // Pamiêtaj o tym, jeœli u¿ywasz TextMeshPro
+
+public class NPCDialogue : MonoBehaviour
+{
+    public GameObject dialoguePanel;  // Panel dialogowy
+    public TMP_Text dialogueText;     // Tekst dialogu (TextMeshPro)
+    public string[] dialogueLines;    // Linie dialogu
+
+    private int currentLine = 0;
+    private bool playerNearby = false;
+
+    void Update()
+    {
+        if (playerNearby && Input.GetKeyDown(KeyCode.E))
+        {
+            if (!dialoguePanel.activeSelf)
+            {
+                dialoguePanel.SetActive(true);
+                currentLine = 0;
+                ShowLine();
+            }
+            else
+            {
+                currentLine++;
+                if (currentLine < dialogueLines.Length)
+                {
+                    ShowLine();
+                }
+                else
+                {
+                    dialoguePanel.SetActive(false);
+                }
+            }
+        }
+    }
+
+    void ShowLine()
+    {
+        dialogueText.text = dialogueLines[currentLine];
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerNearby = true;
+            // Tu mo¿esz dodaæ np. podpowiedŸ "Naciœnij E, aby rozmawiaæ"
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerNearby = false;
+            dialoguePanel.SetActive(false);
+        }
+    }
+}
+
